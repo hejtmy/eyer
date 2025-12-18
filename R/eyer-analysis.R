4f7e8231793f37933b82b78b2db64bf15a758c5f
Lukáš Hejtmánek <hejtmy@gmail.com>
2025-12-18 18:49:31 +0100

    Cleans out the code

--- R/eyer-analysis.R
+++ R/eyer-analysis.R
@@ -1,28 +1,32 @@
 #' Analyses time spent in each area
 #'
-#' @details returns summative data froma with columns: area, n, duration, duration.na, ratio
-#' @description the totals int he data frame do not correspond to the total row, as the NA data are omited.
+#' @details returns summative data froma with columns: area, n,
+#' duration, duration.na, ratio
+#' @description the totals int he data frame do not correspond to
+#' the total row, as the NA data are omited.
 #'
 #' @param obj \code{\link{EyerObject}}
-#' @param areas list of \code{\link{AreaObject}}. Only necessary if the areas haven\'t been added previously
-#' with the \code{\link{add_area_column}}. If areas are present, the column is recalculated.
+#' @param areas list of \code{\link{AreaObject}}. Only necessary if
+#' the areas haven\'t been added previously
+#' with the \code{\link{add_area_column}}. If areas are present, 
+#' the column is recalculated.
 #'
 #' @return data.frame with calculated information
 #' @export
 #'
 #' @examples
-analyse_fixation_areas <- function(obj, areas = list()){
-  if(length(areas) != 0) obj <- add_area_column(obj, areas)
+analyse_fixation_areas <- function(obj, areas = list()) {
+  if (length(areas) != 0) obj <- add_area_column(obj, areas)
   fixations <- obj$data$fixations
   areas <- unique(fixations$area[!is.na(fixations$area)])
-  total <-sum(fixations$duration, na.rm=TRUE)
+  total <- sum(fixations$duration, na.rm=TRUE)
   df <- data.frame(area = "total",
                    n = nrow(fixations),
                    duration = total,
                    duration.na = sum(is.na(fixations$duration)),
                    ratio = 1,
                    stringsAsFactors = FALSE)
-  for(area in areas){
+  for (area in areas) {
     ls <- list(area = area)
     area_fixations <- fixations[fixations$area == area & !is.na(fixations$area), ]
     ls$n <- nrow(area_fixations)
@@ -47,7 +51,7 @@ analyse_fixation_areas <- function(obj, areas = list()){
 #' @export
 #'
 #' @examples
-analyse_gaze_areas <- function(obj, areas = list()){
+analyse_gaze_areas <- function(obj, areas = list()) {
   if(length(areas) != 0) obj <- add_area_column(obj, areas)
   gaze <- obj$data$gaze
   areas <- unique(gaze$area[!is.na(gaze$area)])
